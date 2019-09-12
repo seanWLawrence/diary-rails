04b4fe53090868352a5ddb8928b436fb53a4e6d3
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-12 08:26:15 -0700

    basic entries route setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
-  post "/graphql", to: "graphql#execute"
+  post '/graphql', to: 'graphql#execute'
+
+  get '/entries(*all)', to: 'entries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
