2e7af2a207406ba237448c3dc6572a7defdec6e8
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-14 14:18:28 -0700

    add randomize date trait

--- cypress/ruby/entries_seed.rb
+++ cypress/ruby/entries_seed.rb
@@ -1,1 +1,1 @@
-FactoryBot.create_list :entry, 10
+FactoryBot.create_list :entry, 10, :random_created_at
--- test/factories/entry.rb
+++ test/factories/entry.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     affirmations { ['some affirmation', 'some other affirmation'] }
     positive_experiences { ['some positive_experience', 'some other positive_experience'] }
     improvements { ['some improvement', 'some other improvement'] }
+
+    trait :random_created_at do
+      created_at { Time.at(rand * Time.now.to_i) }
+    end
   end
 end
