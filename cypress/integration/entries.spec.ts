76136168ea232511ee6553222e8d4e08966388d9
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-14 15:31:49 -0700

    fix failing spec

--- cypress/integration/entries.spec.ts
+++ cypress/integration/entries.spec.ts
@@ -2,6 +2,7 @@
 
 describe('entries', () => {
   it('allows you to toggle entries open/collapsed', () => {
+    cy.viewport('iphone-5');
     cy.callRuby('./entries_seed.rb');
     cy.visit('/entries');
 
@@ -27,16 +28,16 @@ describe('entries', () => {
      * Toggle individual entry
      */
 
-    cy.findAllByText(/.*\+/g)
-      .first()
-      .next()
-      .click();
-
-    cy.findByText(/estoy agradecido por/gi);
-    cy.findByText(/¿Qué haría grandioso hoy/gi);
-    cy.findByText(/Estoy\.\.\./i);
-    cy.findByText(/Cosas increíbles que sucedieron hoy/gi);
-    cy.findByText(/¿Cómo podría haber mejorado aún más hoy/gi);
+    cy.get('section').within(() => {
+      cy.findAllByText(/.*\+/g)
+        .first()
+        .click();
+      cy.findByText(/estoy agradecido por/gi);
+      cy.findByText(/¿Qué haría grandioso hoy/gi);
+      cy.findByText(/Estoy\.\.\./i);
+      cy.findByText(/Cosas increíbles que sucedieron hoy/gi);
+      cy.findByText(/¿Cómo podría haber mejorado aún más hoy/gi);
+    });
 
     cy.get('section')
       .first()
