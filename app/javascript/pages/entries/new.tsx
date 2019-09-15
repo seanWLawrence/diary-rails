8d82a08c0f2e11d9ed87cb52d5d369cc6b370cfe
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-14 22:17:07 -0700

    create input groups

--- app/javascript/pages/entries/new.tsx
+++ app/javascript/pages/entries/new.tsx
@@ -26,6 +26,63 @@ let UPSERT_ENTRY_MUTATION = gql`
   }
 `;
 
+interface InputGroupProps {
+  setState: React.Dispatch<React.SetStateAction<NewEntryState>>;
+  state: NewEntryState;
+  name: string;
+  label: string;
+}
+
+let InputGroup: FC<InputGroupProps> = ({ setState, state, name, label }) => {
+  return (
+    <>
+      {state[name].map((value, index) => {
+        let isLastInput = state[name].length - 1 === index;
+
+        return (
+          <label className="new-entry__label" key={index}>
+            {label} #{index + 1}
+            <input
+              type="text"
+              className="new-entry__input"
+              value={value}
+              required
+              onChange={v =>
+                setState({
+                  ...state,
+                  [name]: state[name].map(
+                    (existingValue: string, existingIndex: number) => {
+                      return index === existingIndex
+                        ? value.target.value
+                        : existingValue;
+                    }
+                  ),
+                })
+              }
+            />
+            {isLastInput && (
+              <div className="new-entry__button-wrapper">
+                <button
+                  type="button"
+                  className="new-entry__button--add"
+                  onClick={() =>
+                    setState({
+                      ...state,
+                      [name]: [...state[name], ''],
+                    })
+                  }
+                >
+                  Add new
+                </button>
+              </div>
+            )}
+          </label>
+        );
+      })}
+    </>
+  );
+};
+
 interface NewEntryState {
   gratitudes: string[];
   goals: string[];
@@ -43,10 +100,8 @@ export let NewEntry: FC<{}> = () => {
     improvements: [''],
   });
 
-  console.log(newEntry.gratitudes);
-
   return (
-    <main className="main__wrapper">
+    <main className="new-entry__main-wrapper">
       <Link to="/entries" className="title__anchor">
         <h1 className="title">Diario de 5 minutos</h1>
       </Link>
@@ -60,54 +115,52 @@ export let NewEntry: FC<{}> = () => {
       <form className="new-entry__form">
         <h2 className="new-entry__title">Estoy agradecido por...</h2>
 
-        {newEntry.gratitudes.map((gratitude: string, index: number) => {
-          let isLastInput = newEntry.gratitudes.length - 1 === index;
-          return (
-            <label className="new-entry__label" key={index}>
-              Agradecido #{index + 1}
-              <input
-                type="text"
-                className="new-entry__input"
-                value={gratitude}
-                required
-                onChange={v =>
-                  setNewEntry({
-                    ...newEntry,
-                    gratitudes: newEntry.gratitudes.map(
-                      (g: string, i: number) => {
-                        return i === index ? v.target.value : g;
-                      }
-                    ),
-                  })
-                }
-              />
-              {isLastInput && (
-                <div className="new-entry__button-wrapper">
-                  <button
-                    type="button"
-                    className="new-entry__button--add"
-                    onClick={() =>
-                      setNewEntry({
-                        ...newEntry,
-                        gratitudes: [...newEntry.gratitudes, ''],
-                      })
-                    }
-                  >
-                    Add new
-                  </button>
-                </div>
-              )}
-            </label>
-          );
-        })}
-        <label className="new-entry__label">¿Qué haría grandioso hoy?</label>
-        <label className="new-entry__label">Estoy...</label>
-        <label className="new-entry__label">
+        <InputGroup
+          name="gratitudes"
+          setState={setNewEntry}
+          state={newEntry}
+          label="Agradecio"
+        />
+
+        <h2 className="new-entry__title">¿Qué haría grandioso hoy?</h2>
+
+        <InputGroup
+          name="goals"
+          setState={setNewEntry}
+          state={newEntry}
+          label="Metra"
+        />
+
+        <h2 className="new-entry__title">Estoy...</h2>
+
+        <InputGroup
+          name="affirmations"
+          setState={setNewEntry}
+          state={newEntry}
+          label="Affirma"
+        />
+
+        <h2 className="new-entry__title">
           Cosas increíbles que sucedieron hoy...
-        </label>
-        <label className="new-entry__label">
+        </h2>
+
+        <InputGroup
+          name="positiveExperiences"
+          setState={setNewEntry}
+          state={newEntry}
+          label="Increible cosa"
+        />
+
+        <h2 className="new-entry__title">
           ¿Cómo podría haber mejorado aún más hoy?
-        </label>
+        </h2>
+
+        <InputGroup
+          name="improvements"
+          setState={setNewEntry}
+          state={newEntry}
+          label="Mejorado"
+        />
       </form>
     </main>
   );
