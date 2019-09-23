cb1e49fe39007275a943bbe42e83b9eaf9623687
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-22 20:29:19 -0700

    refactor new entry to use extracted entry form

--- app/javascript/pages/entries/new.tsx
+++ app/javascript/pages/entries/new.tsx
@@ -1,14 +1,6 @@
-import { useMutation } from '@apollo/react-hooks';
-import React, { FC, FormEvent, useState } from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-
-import InputGroup from '../../components/input-group';
-
-import ENTRIES_QUERY, { EntriesQuery } from './entries.graphql';
-import UPSERT_ENTRY_MUTATION, {
-  UpsertEntryMutation,
-  UpsertEntryMutationVariables,
-} from './upsert-entry.graphql';
+import EntryForm from './form';
 
 import './new.sass';
 
@@ -18,68 +10,7 @@ interface NewEntryProps {
   };
 }
 
-interface NewEntryState {
-  affirmations: string[];
-  goals: string[];
-  gratitudes: string[];
-  improvements: string[];
-  positiveExperiences: string[];
-}
-
-let onSubmit = (fn: (val?: any) => void) => (
-  event: FormEvent<HTMLFormElement>,
-) => {
-  event.preventDefault();
-
-  fn();
-};
-
 export let NewEntry: FC<NewEntryProps> = ({ history: { push } }) => {
-  let [newEntry, setNewEntry] = useState<NewEntryState>({
-    affirmations: [''],
-    goals: [''],
-    gratitudes: [''],
-    improvements: [''],
-    positiveExperiences: [''],
-  });
-
-  let [viewAllFields, setViewAllFields] = useState(false);
-
-  let [upsertEntry] = useMutation<
-    UpsertEntryMutation,
-    UpsertEntryMutationVariables
-  >(UPSERT_ENTRY_MUTATION, {
-    variables: newEntry,
-    update(
-      cache,
-      {
-        data: {
-          upsertEntry: { success, entry },
-        },
-      },
-    ) {
-      if (success) {
-        try {
-          let data: EntriesQuery = cache.readQuery({ query: ENTRIES_QUERY });
-
-          data.entries = [...data.entries, entry];
-
-          cache.writeQuery({
-            data,
-            query: ENTRIES_QUERY,
-          });
-        } catch (_) {
-          cache.writeQuery({
-            data: { entries: [entry] },
-            query: ENTRIES_QUERY,
-          });
-        } finally {
-          push('/entries');
-        }
-      }
-    },
-  });
-
   return (
     <main className="new-entry__main-wrapper">
       <Link to="/entries" className="entries__logo">
@@ -96,78 +27,7 @@ export let NewEntry: FC<NewEntryProps> = ({ history: { push } }) => {
         </a>
       </nav>
 
-      <form className="new-entry__form" onSubmit={onSubmit(upsertEntry)}>
-        <h2 className="new-entry__title">Estoy agradecido por...</h2>
-
-        <InputGroup
-          name="gratitudes"
-          setState={setNewEntry}
-          state={newEntry}
-          label="Agradecio"
-          required
-        />
-
-        <h2 className="new-entry__title">¿Qué haría grandioso hoy?</h2>
-
-        <InputGroup
-          name="goals"
-          setState={setNewEntry}
-          state={newEntry}
-          label="Metra"
-          required
-        />
-
-        <h2 className="new-entry__title">Estoy...</h2>
-
-        <InputGroup
-          name="affirmations"
-          setState={setNewEntry}
-          state={newEntry}
-          label="Affirma"
-          required
-        />
-
-        {viewAllFields && (
-          <>
-            <h2 className="new-entry__title">
-              Cosas increíbles que sucedieron hoy...
-            </h2>
-            <InputGroup
-              name="positiveExperiences"
-              setState={setNewEntry}
-              state={newEntry}
-              label="Increible cosa"
-            />
-            <h2 className="new-entry__title">
-              ¿Cómo podría haber mejorado aún más hoy?
-            </h2>
-            <InputGroup
-              name="improvements"
-              setState={setNewEntry}
-              state={newEntry}
-              label="Mejorado"
-            />
-          </>
-        )}
-
-        <div
-          className={`new-entry__button-wrapper--${
-            viewAllFields ? 'center' : 'space-between'
-          }`}
-        >
-          {!viewAllFields && (
-            <button
-              className="new-entry__button--view-all-fields"
-              type="button"
-              onClick={() => setViewAllFields(true)}
-            >
-              View all fields
-            </button>
-          )}
-
-          <button className="new-entry__button--save">Save</button>
-        </div>
-      </form>
+      <EntryForm push={push} />
     </main>
   );
 };
